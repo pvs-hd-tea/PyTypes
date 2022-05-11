76a85e1ed92d29a0b5810d96115e991b4d55eb20
Recurved Bow <bowrecurved@gmail.com>
2022-05-11 22:27:47 +0300

    + Dummy code to test github action.

---
+++ dummy_test.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    '''Sample function used for testing. Adds two numbers and returns its sum.'''
+    z = x + y
+    return z
+
+
+def if_add_is_called_with_two_numbers_sum_is_returned():
+    argument1 = 5
+    argument2 = 16
+    expected = 21
+
+    actual = add(argument1, argument2)
+
+    assert expected == actual
