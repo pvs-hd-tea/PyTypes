bf6129bffb96533d2032056060eb729080883954
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-05-24 16:25:00 +0200

    Remove placeholder file

--- tracing/__init__.py
+++ tracing/__init__.py
@@ -1,5 +1,4 @@
-from .example import add
 from .trace_data_category import TraceDataCategory
 from .tracer import Tracer
 
-__all__ = [add.__name__, Tracer.__name__, TraceDataCategory.__name__]
+__all__ = [Tracer.__name__, TraceDataCategory.__name__]
--- tracing/example.py
+++
@@ -1,5 +0,0 @@
-def add(x, y):
-    '''Sample function used for testing.
-    Adds two numbers and returns its sum.'''
-    z = x + y
-    return z
