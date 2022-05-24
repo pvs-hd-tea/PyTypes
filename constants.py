3ef935b3dbb7bdce3e41c523c547d4db3fa47693
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-05-24 16:21:37 +0200

    Separate column constants into namespaced constants

--- constants.py
+++ constants.py
@@ -1,3 +1,21 @@
 # Tracing
-PROJECT_NAME = 'PyTypes'
-TRACE_DATA_COLUMNS = ['Filename', 'Function Name', 'Line Number', 'Category', 'Name', 'Type']
+PROJECT_NAME = "PyTypes"
+
+
+class TraceData:
+    FILENAME = "Filename"
+    FUNCNAME = "Function Name"
+    LINENO = "Line Number"
+    CATEGORY = "Category"
+    VARNAME = "Name"
+    VARTYPE = "Type"
+
+
+TRACE_DATA_COLUMNS = [
+    TraceData.FILENAME,
+    TraceData.FUNCNAME,
+    TraceData.LINENO,
+    TraceData.CATEGORY,
+    TraceData.VARNAME,
+    TraceData.VARTYPE,
+]
