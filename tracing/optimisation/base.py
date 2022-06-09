0a17968e6ee5390f9b897e56fc8d4a7d6f318aa9
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-09 19:24:48 +0200

    Document base class abstractions

--- tracing/optimisation/base.py
+++ tracing/optimisation/base.py
@@ -1,10 +1,22 @@
 from .enums import TriggerStatus
+from .utils import FrameWithLine
 from abc import abstractmethod
+
+import pandas as pd
+
 
 class Optimisation:
     """Base class for tracing-oriented optimisations"""
 
+    def __init__(self, frame):
+        self.frame = frame
+
     def apply(self, frame):
+        """
+        Apply the derived optimization to the given frame.
+        Whether tracing is turned off for a given line depends on the
+        optimisation's status
+        """
         if (status := self.status()) in (TriggerStatus.ENTRY, TriggerStatus.ONGOING):
             frame.f_trace_lines = False
         else:
@@ -13,12 +25,24 @@ class Optimisation:
 
     @abstractmethod
     def status(self) -> TriggerStatus:
+        """
+        Get the optimization's status.
+        See TriggerStatus for an explanation of how each one
+        influences the impact of the optimization upon a frame in self.apply
+        """
         pass
 
     @abstractmethod
-    def advance(self, current_frame) -> None:
+    def advance(self, current_frame: FrameWithLine, traced: pd.DataFrame) -> None:
+        """
+        Modify the optimization's internal state based on given frame
+        """
         pass
 
     @abstractmethod
     def __eq__(self, o: object) -> bool:
+        """
+        Overloaded equality in order to check for already 
+        active / duplicate optimizations
+        """
         pass
