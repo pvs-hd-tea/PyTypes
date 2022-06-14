b79617c06c201964e187a38ae1f55c807d662528
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-15 00:31:39 +0200

    Add Project class for simpler repo interactions

---
+++ fetching/projio.py
@@ -0,0 +1,9 @@
+import pathlib
+
+class Project:
+    """
+    Represents a downloaded Python repository. 
+    Use for more intuitive navigation of the project 
+    """
+    def __init__(self, root: pathlib.Path):
+        self.root = root
