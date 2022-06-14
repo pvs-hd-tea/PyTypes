246a218c3605375c94eaaeb722bf28426a13523c
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-14 16:38:19 +0200

    Make base classes abstract

--- fetching/base.py
+++ fetching/base.py
@@ -1,12 +1,12 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 import pathlib
 
 
-class ApplicationStrategy:
+class ApplicationStrategy(ABC):
     pass
 
 
-class TestDetector:
+class TestDetector(ABC):
     """
     When given the path to a repo, attempts to detect a specific
     testing suite and create the appropriate application strategy
@@ -14,7 +14,8 @@ class TestDetector:
     """
 
     def __init__(self, path2project: pathlib.Path):
-        # TODO: Should this take an instance of repository?
+        # TODO: Should this take an instance of repository? 
+        # TODO: Perhaps a project class is needed, in order to interact better with the project structure
         self.path2project = path2project
 
     @abstractmethod
