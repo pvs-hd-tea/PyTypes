97d6a7970255e1417f5f7218eaa1162e2f5bcc15
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-14 23:48:22 +0200

    Document ApplicationStrategy

--- fetching/base.py
+++ fetching/base.py
@@ -3,6 +3,12 @@ import pathlib
 
 
 class ApplicationStrategy(ABC):
+    """
+    Implement for a specific test framework; 
+    When given a file that uses the specified framework,
+    parse this file and insert code that will cause the test functions to be traced 
+    upon execution
+    """
     pass
 
 
