689425c2f021e51e6103113d408d6b33cc946356
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-15 01:41:32 +0200

    Formatting

--- fetching/projio.py
+++ fetching/projio.py
@@ -1,9 +1,11 @@
 import pathlib
 
+
 class Project:
     """
-    Represents a downloaded Python repository. 
-    Use for more intuitive navigation of the project 
+    Represents a downloaded Python repository.
+    Use for more intuitive navigation of the project
     """
+
     def __init__(self, root: pathlib.Path):
         self.root = root
