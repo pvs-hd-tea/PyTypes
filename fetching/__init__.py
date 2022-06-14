2c625d124bd9f3edf94083a464e1402778e02536
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-14 16:39:08 +0200

    Add command line interface for fetching

--- fetching/__init__.py
+++ fetching/__init__.py
@@ -1,3 +1,37 @@
-from .repo import repository_factory, Repository
+import pathlib
+
+import click
+
+from .repo import repository_factory, Repository, GitRepository, ArchiveRepository
 
 __all__ = [repository_factory.__name__, Repository.__name__]
+
+
+@click.command(name="fetch", help="download repositories and apply tracing decorators")
+@click.option(
+    "-u",
+    "--url",
+    type=str,
+    help="URL to a repository",
+    required=True,
+)
+@click.option(
+    "-f",
+    "--format",
+    type=click.Choice([GitRepository.fmt, ArchiveRepository.fmt], case_sensitive=False),
+    help="Indicate repository format explicitly",
+    required=False,
+    default=None,
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(exists=False, dir_okay=True, writable=True, path_type=pathlib.Path),
+    help="Download output path",
+    required=True,
+)
+def main(**params):
+    url, fmt, out = params["url"], params.get["format"], params["output"]
+
+    repo = repository_factory(project_url=url, fmt=fmt)
+    repo.fetch(out)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    pass
+import click
 
+import fetching
+import tracing
 
 if __name__ == "__main__":
+    # Ordered by workflow usage
+    main = click.Group(commands=[fetching.main])
     main()
