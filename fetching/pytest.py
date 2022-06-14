debc85c60315221194176900f1abb787d5be7c84
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-14 23:48:55 +0200

    Support pyproject toml files

--- fetching/pytest.py
+++ fetching/pytest.py
@@ -1,5 +1,7 @@
 import pathlib
 
+import toml
+
 from .base import ApplicationStrategy, TestDetector
 
 
@@ -8,13 +10,33 @@ class PyTestDetector(TestDetector):
         super().__init__(path2project)
 
     def matches(self) -> bool:
-        pytest_config = self.path2project / "pytest.ini"
-        if pytest_config.is_file():
+        if self._has_pytest_ini():
             return True
 
-        # TODO: support pyproject.toml
+        if self._has_pytest_in_pyproject():
+            return True
 
         return False
+
+    def create_strategy(self) -> ApplicationStrategy:
+        return PyTestStrategy()
+
+    def _has_pytest_ini(self) -> bool:
+        pytest_config = self.path2project / "pytest.ini"
+        return pytest_config.is_file()
+
+    def _has_pytest_in_pyproject(self) -> bool:
+        pyproj = self.path2project / "pyproject.toml"
+        if not pyproj.is_file():
+            return False
+
+        pyproj_cfg = toml.load(pyproj.open())
+        return (
+            "tool" in pyproj_cfg
+            and "poetry" in pyproj_cfg["tool"]
+            and "dev-dependencies" in pyproj_cfg["tool"]["poetry"]
+            and "pytest" in pyproj_cfg["tool"]["poetry"]["dev-dependencies"]
+        )
 
 
 class PyTestStrategy(ApplicationStrategy):
