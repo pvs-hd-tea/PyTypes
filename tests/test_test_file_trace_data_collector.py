aea0c94c4084f5c4b498375eb3157ffd3200b00d
Viet Tran Ba <bowrecurved@gmail.com>
2022-06-16 19:45:20 +0200

    + Trace Data Collector now can delete collected trace data files

---
+++ tests/test_test_file_trace_data_collector.py
@@ -0,0 +1,88 @@
+import pathlib
+import pandas as pd
+import pytest
+import constants
+from tracing import TracerDecoratorAppender, TestFileTraceDataCollector
+
+cwd = pathlib.Path.cwd() / 'resource'
+
+
+def delete_trace_data_files_in_tests():
+    """ Deletes all trace data files in tests. """
+    trace_data = pd.DataFrame(columns=constants.TraceData.SCHEMA.keys())
+    trace_data = trace_data.astype(constants.TraceData.SCHEMA)
+
+    file_ending = constants.TRACE_DATA_FILE_ENDING
+    file_pattern = "*" + file_ending
+
+    potential_trace_data_file_paths = cwd.parent.rglob(file_pattern)
+
+    for potential_trace_data_file_path in potential_trace_data_file_paths:
+        potential_trace_data = pd.read_pickle(potential_trace_data_file_path)
+        if (trace_data.dtypes == potential_trace_data.dtypes).all():
+            potential_trace_data_file_path.unlink()
+
+
+def test_if_argument_of_append_decorator_is_none_error_is_raised():
+    test_object = TestFileTraceDataCollector()
+    with pytest.raises(TypeError):
+        test_object.collect_trace_data(None)
+
+
+def test_if_test_object_collects_generated_trace_data_and_keeps_files_it_returns_correct_trace_data_and_files_are_kept():
+    delete_trace_data_files_in_tests()
+
+    expected_trace_data_file_path = [path for path in cwd.rglob("test_file_trace_data_collector_expected_data1.test_pytype")][0]
+    expected_trace_data = pd.read_pickle(expected_trace_data_file_path)
+    expected_trace_data = expected_trace_data.astype(constants.TraceData.SCHEMA)
+    expected_trace_data = expected_trace_data.sort_values(by=['Filename', 'Function Name', 'Line Number'],
+                                                          ignore_index=True)
+    tracer_decorator_appender = TracerDecoratorAppender()
+    tracer_decorator_appender.append_decorator_on_all_files_in(cwd, True)
+    tracer_decorator_appender.execute_decorator_appended_files()
+
+    test_object = TestFileTraceDataCollector()
+    test_object.collect_trace_data(cwd.parent, True, False)
+    actual_trace_data = test_object.trace_data
+    actual_trace_data = actual_trace_data.sort_values(by=['Filename', 'Function Name', 'Line Number'],
+                                                      ignore_index=True)
+    # with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    #    print(actual_trace_data.shape[0])
+    #    print(expected_trace_data.shape[0])
+
+    assert actual_trace_data.shape[0] == 52
+    assert expected_trace_data["Filename"].equals(actual_trace_data["Filename"])
+
+    pytype_trace_data_files = [path for path in cwd.parent.rglob("*" + constants.TRACE_DATA_FILE_ENDING)]
+    assert len(pytype_trace_data_files) == 6
+    delete_trace_data_files_in_tests()
+
+
+def test_if_test_object_collects_generated_trace_data_and_deletes_files_it_returns_correct_trace_data_and_files_are_deleted():
+    delete_trace_data_files_in_tests()
+
+    expected_trace_data_file_path = [path for path in cwd.rglob("test_file_trace_data_collector_expected_data1.test_pytype")][0]
+    expected_trace_data = pd.read_pickle(expected_trace_data_file_path)
+    expected_trace_data = expected_trace_data.astype(constants.TraceData.SCHEMA)
+    expected_trace_data = expected_trace_data.sort_values(by=['Filename', 'Function Name', 'Line Number'],
+                                                          ignore_index=True)
+    tracer_decorator_appender = TracerDecoratorAppender()
+    tracer_decorator_appender.append_decorator_on_all_files_in(cwd, True)
+    tracer_decorator_appender.execute_decorator_appended_files()
+
+    test_object = TestFileTraceDataCollector()
+    test_object.collect_trace_data(cwd.parent, True, True)
+    actual_trace_data = test_object.trace_data
+    actual_trace_data = actual_trace_data.sort_values(by=['Filename', 'Function Name', 'Line Number'],
+                                                      ignore_index=True)
+    # with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    #    print(actual_trace_data.shape[0])
+    #    print(expected_trace_data.shape[0])
+
+    assert actual_trace_data.shape[0] == 52
+    assert expected_trace_data["Filename"].equals(actual_trace_data["Filename"])
+
+    pytype_trace_data_files = [path for path in cwd.parent.rglob("*" + constants.TRACE_DATA_FILE_ENDING)]
+    assert len(pytype_trace_data_files) == 0
+
+    delete_trace_data_files_in_tests()
--- tracing/test_file_trace_data_collector.py
+++ tracing/test_file_trace_data_collector.py
@@ -11,7 +11,8 @@ class TestFileTraceDataCollector:
         self.trace_data = self.trace_data.astype(constants.TraceData.SCHEMA)
 
     def collect_trace_data(self, path: pathlib.Path,
-                           include_also_files_in_subdirectories: bool = False) -> None:
+                           include_also_files_in_subdirectories: bool = False,
+                           delete_collected_files: bool = True) -> None:
         if path is None:
             raise TypeError
         self.trace_data = pd.DataFrame(columns=constants.TraceData.SCHEMA.keys())
@@ -30,6 +31,8 @@ class TestFileTraceDataCollector:
             potential_trace_data = pd.read_pickle(potential_trace_data_file_path)
             if (self.trace_data.dtypes == potential_trace_data.dtypes).all():
                 trace_datas.append(potential_trace_data)
+                if delete_collected_files:
+                    potential_trace_data_file_path.unlink()
 
         if len(trace_datas) > 0:
             self.trace_data = pd.concat(trace_datas, ignore_index=True, sort=False)
