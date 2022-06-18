99f86be9f14d663f3e196aba172393eba19ab931
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-18 23:24:51 +0200

    Add further lookup for test directory

--- fetching/projio.py
+++ fetching/projio.py
@@ -1,4 +1,5 @@
 import pathlib
+import logging
 
 
 class Project:
@@ -8,11 +9,19 @@ class Project:
     """
 
     def __init__(self, root: pathlib.Path):
+        assert root.is_dir()
         self.root = root
 
     @property
     def test_directory(self) -> pathlib.Path | None:
+        # tests folder in root of project?
         if (p := self.root / "tests").is_dir():
+            logging.info(f"Detected test folder in root of project - {p}")
             return p
 
+        if (p := self.root / self.root.name / "tests").is_dir():
+            logging.info(f"Detected test folder in folder by name of project - {p}")
+            return p
+
+        logging.warning(f"Unable to find test directory for project at {self.root}")
         return None
