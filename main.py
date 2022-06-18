aa5ab6cf120d000df9519c483ce41800740d4356
Viet Tran Ba <bowrecurved@gmail.com>
2022-06-18 21:51:51 +0200

    Checking if mypy catches the None argument

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import click
 
 import fetching
+from tracing import TracerDecoratorAppender
 
 if __name__ == "__main__":
     # Ordered by workflow usage
+    TracerDecoratorAppender().append_decorator_on_all_files_in(None)
     main = click.Group(commands=[fetching.main])
     main()
