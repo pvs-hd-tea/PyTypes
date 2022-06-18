17d8e2dfc225f987c3bddceed31b0a81751e4969
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-18 23:49:43 +0200

    cwd pointed to the wrong path. our tests need to be run from the root

--- tests/test_decorator_appender.py
+++ tests/test_decorator_appender.py
@@ -6,7 +6,7 @@ from fetching.projio import Project
 
 from fetching.strat import PyTestStrategy
 
-cwd = pathlib.Path.cwd() / "resource" / "sample_decorator_appender_folder"
+cwd = pathlib.Path.cwd() / "tests" / "resource" / "sample_decorator_appender_folder"
 
 
 @pytest.fixture()
@@ -28,12 +28,14 @@ def test_if_test_object_searches_for_test_files_in_folders_it_generates_test_fil
     test_object.apply(project_folder)
     test_file_paths = cwd.glob("test_*.py")
     new_test_file_paths = [
-        pathlib.Path(str(file_path).replace(".py", test_object.file_ending))
+        pathlib.Path(str(file_path).replace(".py", test_object.APPENDED_FILEPATH))
         for file_path in test_file_paths
-        if not str(file_path).endswith(test_object.file_ending)
+        if not str(file_path).endswith(test_object.APPENDED_FILEPATH)
     ]
+    assert len(new_test_file_paths) != 0
+
     for new_test_file_path in new_test_file_paths:
-        assert pathlib.Path.exists(new_test_file_path)
+        assert new_test_file_path.exists()
 
 
 def test_if_test_object_searches_for_test_files_in_folders_including_subfolders_it_generates_test_files(
@@ -43,9 +45,11 @@ def test_if_test_object_searches_for_test_files_in_folders_including_subfolders_
     test_object.apply(project_folder)
     test_file_paths = cwd.rglob("test_*.py")
     new_test_file_paths = [
-        pathlib.Path(str(file_path).replace(".py", test_object.file_ending))
+        pathlib.Path(str(file_path).replace(".py", test_object.APPENDED_FILEPATH))
         for file_path in test_file_paths
-        if not str(file_path).endswith(test_object.file_ending)
+        if not str(file_path).endswith(test_object.APPENDED_FILEPATH)
     ]
+    assert len(new_test_file_paths) != 0
+
     for new_test_file_path in new_test_file_paths:
-        assert pathlib.Path.exists(new_test_file_path)
+        assert new_test_file_path.exists()
