a3b48b5b46f6051bbccc3ad7b626b2fa00a5a33f
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-19 16:14:52 +0200

    Use better name

--- fetching/projio.py
+++ fetching/projio.py
@@ -16,13 +16,13 @@ class Project:
     @functools.cached_property
     def test_directory(self) -> pathlib.Path | None:
         # tests folder in root of project?
-        if (p := self.root / "tests").is_dir():
-            logging.info(f"Detected test folder in root of project - {p}")
-            return p
+        if (test_dir := self.root / "tests").is_dir():
+            logging.info(f"Detected test folder in root of project - {test_dir}")
+            return test_dir
 
-        if (p := self.root / self.root.name / "tests").is_dir():
-            logging.info(f"Detected test folder in folder by name of project - {p}")
-            return p
+        if (test_dir := self.root / self.root.name / "tests").is_dir():
+            logging.info(f"Detected test folder in folder by name of project - {test_dir}")
+            return test_dir
 
         logging.warning(f"Unable to find test directory for project at {self.root}")
         return None
