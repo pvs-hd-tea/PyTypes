bf6b9cd65c6ecc77e608f51a268636198805e42c
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-06-20 17:33:44 +0200

    Add pandas test

--- tests/fetching/test_archiverepo.py
+++ tests/fetching/test_archiverepo.py
@@ -21,3 +21,21 @@ def test_numpy_zip_is_archive():
     assert (np_path / "pyproject.toml").is_file()
 
     repoutils.delete_repo(np_path)
+
+
+def test_pandas_zip_is_archive():
+    pandas_path = repoutils.create_repo("pandas-zip")
+
+    np_repo = fetching.Repository.factory(
+        r"https://github.com/pandas-dev/pandas/archive/refs/heads/main.zip"
+    )
+    assert isinstance(
+        np_repo, fetching.ArchiveRepository
+    ), f"Failed to detect pandas archive URL as such"
+
+    np_repo.fetch(pandas_path)
+
+    # Basic check for contents of repo, requires knowledge thereof
+    assert (pandas_path / "pyproject.toml").is_file()
+
+    repoutils.delete_repo(pandas_path)
