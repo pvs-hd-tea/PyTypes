b324a287dc6920e8a2116590f0782d36d454e1f7
Recurved Bow <bowrecurved@gmail.com>
2022-06-30 21:31:44 +0200

    Rerunning pipeline

--- tests/test_trace_data_file_collector.py
+++ tests/test_trace_data_file_collector.py
@@ -7,7 +7,7 @@ from tracing import TraceDataFileCollector
 cwd = pathlib.Path.cwd() / "tests" / "resource" / "external" / "PyTypes_BinaryFiles" / "sample_trace_data_files"
 
 
-def test_if_test_object_collects_generated_trace_data_in_folder_and_subfolders_and_keeps_files_it_returns_correct_trace_data_and_files_are_kept():
+def test_if_test_object_collects_generated_trace_data_in_folder_and_subfolders_and_keeps_files_it_returns_correct_trace_data():
     expected_trace_data_file_path = cwd / "samples_rglob.test_pytype"
     expected_trace_data = pd.read_pickle(expected_trace_data_file_path)
     expected_trace_data = expected_trace_data.astype(constants.TraceData.SCHEMA)
@@ -25,7 +25,7 @@ def test_if_test_object_collects_generated_trace_data_in_folder_and_subfolders_a
     assert expected_trace_data.equals(actual_trace_data)
 
 
-def test_if_test_object_collects_generated_trace_data_in_folder_it_returns_correct_trace_data_and_files_are_deleted():
+def test_if_test_object_collects_generated_trace_data_in_folder_it_returns_correct_trace_data():
     expected_trace_data_file_path = cwd / "samples_glob.test_pytype"
     expected_trace_data = pd.read_pickle(expected_trace_data_file_path)
     expected_trace_data = expected_trace_data.astype(constants.TraceData.SCHEMA)
