6ccfaaa95538437d329e6b897a242dba702f0110
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-07-29 17:31:53 +0200

    Resource code for assigning to variables

---
+++ tests/resource/typegen/assignments.py
@@ -0,0 +1,21 @@
+# Simple assigment, is lifted to AnnAssign
+z = 0
+# Resulting from math op, shouldnt differ from simple assignment
+y = 5.0 + z
+
+# data structures
+d = dict(zip("HelloWorld", range(10)))
+s = set(range(10))
+l = list(range(10))
+
+
+# annotations in other chained and aug assignments are not (directly) supported 
+# solution: declare variable beforehand
+# a: float
+# b: int
+# i: float
+# j: int
+(a, b), (i, j) = y, z, y, z
+
+f = y = 10
+f += y - 20
