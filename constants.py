affe3dd23483da02818283dd6080ebc018a35250
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-06 17:24:56 +0200

    Document schema properly

--- constants.py
+++ constants.py
@@ -26,13 +26,31 @@ class TraceData:
     VARTYPE = "Type"
 
     SCHEMA = {
+        # relative path to file of variable,
+        # from project root
         FILENAME: pd.StringDtype(),
+        # module of class this variable is in.
+        # None if not in a class' scope
         CLASS_MODULE: pd.StringDtype(),
+        # name of class this variable is in.
+        # None if not in a class' scope
         CLASS: pd.StringDtype(),
+        # module of function this variable is in
+        # None if not in a function's scope
         FUNCNAME: pd.StringDtype(),
+        # line number the variable occurs on
         LINENO: pd.UInt64Dtype(),
+        # number identifying context said variable appears in
+        # See TraceDataCategory for more information
         CATEGORY: pd.Int64Dtype(),
+        # name of the variableor, when CATEGORY indicates a FUNCTION_RETURN,
+        # it is the name of the returning function
+        # never None
         VARNAME: pd.StringDtype(),
+        # the module of the variable's type
+        # None if it is a builtin type
         VARTYPE_MODULE: pd.StringDtype(),
+        # the name of the variable's type
+        # never None
         VARTYPE: pd.StringDtype(),
     }
