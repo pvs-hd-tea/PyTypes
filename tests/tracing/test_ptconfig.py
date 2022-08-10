ea8a6d5493deb8a8de3b015a752e0b80843925ee
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-10 05:56:05 +0200

    Add parsing of filters

---
+++ tests/tracing/test_ptconfig.py
@@ -0,0 +1,38 @@
+import pathlib
+
+from tracing import ptconfig
+
+
+config_path = pathlib.Path("tests", "resource", "configs", "example.toml")
+
+
+def test_pytypes():
+    config = ptconfig.load_config(config_path)
+
+    assert config.pytypes.project == "PyTypes"
+    assert config.pytypes.stdlib_path == pathlib.Path("/", "usr", "lib", "python3.10")
+    assert config.pytypes.proj_path == pathlib.Path(
+        "/", "home", "benji", "Documents", "Uni", "heidelberg", "04", "pytype", "repo"
+    )
+    assert config.pytypes.venv_path == pathlib.Path(
+        "/", "home", "benji", ".cache", "pypoetry", "virtualenvs", "pytypes-xvtnrWJT-py3.10"
+    )
+
+    assert len(config.unifier) == 6
+
+    assert config.unifier[0].kind == "dedup"
+
+    assert config.unifier[1].kind == "drop_test"
+    assert config.unifier[1].test_name_pat == "test_"
+
+    assert config.unifier[2].kind == "drop_mult_var"
+    assert config.unifier[2].min_amount_types_to_drop == 2
+
+    assert config.unifier[3].kind == "drop_mult_var"
+    assert config.unifier[3].min_amount_types_to_drop == 5
+
+    assert config.unifier[4].kind ==  "repl_subty"
+    assert config.unifier[4].only_replace_if_base_was_traced == False
+
+    assert config.unifier[5].kind ==  "repl_subty"
+    assert config.unifier[5].only_replace_if_base_was_traced == True
--- tracing/ptconfig.py
+++ tracing/ptconfig.py
@@ -1,32 +1,79 @@
 from dataclasses import dataclass, field, asdict
 import pathlib
+import typing
 
 import dacite
+from dacite.exceptions import DaciteError
 import toml
 
 import constants
 
 
 @dataclass
-class Config:
+class PyTypes:
     project: str
+
+    stdlib_path: pathlib.Path
+    proj_path: pathlib.Path
+    venv_path: pathlib.Path
+
     output_template: str = field(
         default="pytypes/{project}/{func_name}" + constants.TRACE_DATA_FILE_ENDING
     )
 
 
 @dataclass
-class PyTypesToml:
-    pytypes: Config
+class Dedup:
+    kind: typing.Literal["dedup"] = "dedup"
 
 
-def _load_config(config_path: pathlib.Path) -> PyTypesToml:
+@dataclass
+class DropTest:
+    test_name_pat: str
+    kind: typing.Literal["drop_test"] = "drop_test"
+
+
+@dataclass
+class DropVars:
+    kind: typing.Literal["drop_mult_var"] = "drop_mult_var"
+    min_amount_types_to_drop: int | None = 2
+
+
+@dataclass
+class ReplaceSubtypes:
+    kind: typing.Literal["repl_subty"] = "repl_subty"
+    only_replace_if_base_was_traced: bool | None = False
+
+
+
+# https://github.com/konradhalas/dacite/pull/184
+# the cooler union syntax is not supported
+@dataclass
+class TomlCfg:
+    pytypes: PyTypes
+    unifier: list[typing.Union[Dedup, DropTest, DropVars, ReplaceSubtypes]]
+
+
+def load_config(config_path: pathlib.Path) -> TomlCfg:
     cfg = toml.load(config_path.open())
-    return dacite.from_dict(
-        data_class=PyTypesToml, data=cfg, config=dacite.Config(strict=True)
-    )
 
+    try:
+        return dacite.from_dict(
+            data_class=TomlCfg,
+            data=cfg,
+            config=dacite.Config(
+                cast=[pathlib.Path],
+                strict=True,
+                strict_unions_match=True,
+            ),
+        )
 
-def _write_config(config_path: pathlib.Path, pttoml: PyTypesToml):
+    except DaciteError as e:
+        print(f"Failed to load config from {config_path}. Here is the schema:\n")
+        raise e
+
+def write_config(config_path: pathlib.Path, pttoml: TomlCfg):
     config_path.parent.mkdir(parents=True, exist_ok=True)
-    toml.dump(asdict(pttoml), config_path.open("w"))
+    ad = asdict(pttoml)
+    ad["pytypes"].pop("output_template")
+    toml.dump(ad, config_path.open("w"))
