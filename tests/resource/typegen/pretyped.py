ee99d23d0d4bcb4240fc71034421ccced46aae69
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-12 20:11:13 +0200

    Fix mypy

--- tests/resource/typegen/pretyped.py
+++ tests/resource/typegen/pretyped.py
@@ -1,9 +1,14 @@
-from tests.resource.typegen.callable import C
+class C:
+    def __init__(self) -> None:
+        self.a = 5
+
+    def m(self, i) -> int:
+        return self.a + i
 
 def f(i: int) -> str:
-    c = C()
-    c.a: int = 10
-    return f"{c.outer(i)}"
+    c: C = C()
+    c.a = 10
+    return f"{c.m(i)}"
 
 def another_one():
     a: int
