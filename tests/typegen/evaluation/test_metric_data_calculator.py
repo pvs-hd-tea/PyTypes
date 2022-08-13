b0483481d368cb53eb046a3066b567b213e740ec
Viet Tran Ba <bowrecurved@gmail.com>
2022-08-13 20:31:36 +0200

    Metric Data Collector now calculates Metric Data using Column Offset instead of Line Number correctly

--- tests/typegen/evaluation/test_metric_data_calculator.py
+++ tests/typegen/evaluation/test_metric_data_calculator.py
@@ -10,7 +10,7 @@ def get_sample_data():
         "sample_original_filename",
         None,
         "sample_function_name",
-        10,
+        0,
         TraceDataCategory.FUNCTION_PARAMETER,
         "parameter",
         int.__name__,
@@ -19,7 +19,7 @@ def get_sample_data():
         "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        25,
+        4,
         TraceDataCategory.LOCAL_VARIABLE,
         "local_variable",
         bool.__name__,
@@ -28,7 +28,25 @@ def get_sample_data():
         "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        35,
+        12,
+        TraceDataCategory.LOCAL_VARIABLE,
+        "local_variable",
+        int.__name__,
+    ]
+    sample_original_data.loc[len(sample_original_data.index)] = [
+        "sample_original_filename",
+        str.__name__,
+        "sample_function_name2",
+        4,
+        TraceDataCategory.LOCAL_VARIABLE,
+        "local_variable",
+        str.__name__,
+    ]
+    sample_original_data.loc[len(sample_original_data.index)] = [
+        "sample_original_filename",
+        str.__name__,
+        "sample_function_name2",
+        0,
         TraceDataCategory.FUNCTION_RETURN,
         "sample_function_name2",
         float.__name__,
@@ -39,26 +57,36 @@ def get_sample_data():
         "sample_generated_filename",
         None,
         "sample_function_name",
-        10,
+        0,
         TraceDataCategory.FUNCTION_PARAMETER,
         "parameter",
         int.__name__,
     ]
     sample_generated_data.loc[len(sample_generated_data.index)] = [
-        "sample_generated_filename",
+        "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        25,
+        4,
         TraceDataCategory.LOCAL_VARIABLE,
         "local_variable",
-        object.__name__,
+        str.__name__,
+    ]
+
+    sample_generated_data.loc[len(sample_generated_data.index)] = [
+        "sample_original_filename",
+        str.__name__,
+        "sample_function_name2",
+        4,
+        TraceDataCategory.LOCAL_VARIABLE,
+        "local_variable",
+        str.__name__,
     ]
 
     sample_generated_data.loc[len(sample_generated_data.index)] = [
         "sample_generated_filename",
         str.__name__,
         "sample_function_name2",
-        45,
+        4,
         TraceDataCategory.LOCAL_VARIABLE,
         "local_variable2",
         str.__name__,
@@ -73,7 +101,7 @@ def test_metric_calculator_returns_correct_metric_data():
         "sample_original_filename",
         None,
         "sample_function_name",
-        10,
+        0,
         TraceDataCategory.FUNCTION_PARAMETER,
         "parameter",
         int.__name__,
@@ -85,11 +113,11 @@ def test_metric_calculator_returns_correct_metric_data():
         "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        25,
+        4,
         TraceDataCategory.LOCAL_VARIABLE,
         "local_variable",
         bool.__name__,
-        object.__name__,
+        str.__name__,
         True,
         False,
     ]
@@ -97,20 +125,43 @@ def test_metric_calculator_returns_correct_metric_data():
         "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        35,
+        12,
+        TraceDataCategory.LOCAL_VARIABLE,
+        "local_variable",
+        int.__name__,
+        None,
+        False,
+        False,
+    ]
+    expected_data.loc[len(expected_data.index)] = [
+        "sample_original_filename",
+        str.__name__,
+        "sample_function_name2",
+        4,
+        TraceDataCategory.LOCAL_VARIABLE,
+        "local_variable",
+        str.__name__,
+        str.__name__,
+        True,
+        True,
+    ]
+    expected_data.loc[len(expected_data.index)] = [
+        "sample_original_filename",
+        str.__name__,
+        "sample_function_name2",
+        0,
         TraceDataCategory.FUNCTION_RETURN,
         "sample_function_name2",
         float.__name__,
         None,
         False,
-        False,
+        False
     ]
-
     expected_data.loc[len(expected_data.index)] = [
         "sample_original_filename",
         str.__name__,
         "sample_function_name2",
-        45,
+        4,
         TraceDataCategory.LOCAL_VARIABLE,
         "local_variable2",
         None,
@@ -118,11 +169,15 @@ def test_metric_calculator_returns_correct_metric_data():
         None,
         None,
     ]
+
     expected_data = expected_data.astype(constants.TraceData.METRICS_SCHEMA)
     sample_original_data, sample_generated_data = get_sample_data()
     test_object = MetricDataCalculator()
     test_object.add_filename_mapping("sample_original_filename", "sample_generated_filename")
     actual_data = test_object.get_metric_data(sample_original_data, sample_generated_data)
+
+    print(actual_data)
+
     assert expected_data.equals(actual_data)
 
 
--- typegen/evaluation/metric_data_calculator.py
+++ typegen/evaluation/metric_data_calculator.py
@@ -7,6 +7,10 @@ import constants
 
 
 class MetricDataCalculator:
+    INDEX = "Index"
+    INDEX2 = "Index 2"
+    CUMCOUNT = "CumCount"
+
     """Calculates the metric data containing the correctness & completeness."""
     def __init__(self):
         self.modified_filenames_by_original: Dict[str, str] = {}
@@ -24,23 +28,63 @@ class MetricDataCalculator:
 
         subset_merged = list(constants.TraceData.TYPE_HINT_SCHEMA.keys())
         subset_merged = subset_merged.copy()
+
+        # Used to sort the values.
+        original_type_hint_data[MetricDataCalculator.INDEX] = original_type_hint_data.index
+        modified_type_hint_data[MetricDataCalculator.INDEX2] = modified_type_hint_data.index
+
+        # Used to treat duplicate rows as separate rows (to merge the dataframes).
+        original_type_hint_data[MetricDataCalculator.CUMCOUNT] = original_type_hint_data.groupby(
+            subset_merged).cumcount()
+        modified_type_hint_data[MetricDataCalculator.CUMCOUNT] = modified_type_hint_data.groupby(
+            subset_merged).cumcount()
+        subset_merged.append(MetricDataCalculator.CUMCOUNT)
+
+        merged_data = pd.merge(original_type_hint_data, modified_type_hint_data,
+                                            on=subset_merged, how='inner')  # type: ignore
+        merged_data = merged_data.drop(MetricDataCalculator.CUMCOUNT, axis=1)
+        merged_data[constants.TraceData.VARTYPE2] = merged_data[constants.TraceData.VARTYPE]
+        merged_data[constants.TraceData.CORRECTNESS] = True
+        merged_data[constants.TraceData.COMPLETENESS] = True
+
+        original_type_hint_data = original_type_hint_data[
+            ~original_type_hint_data[MetricDataCalculator.INDEX].isin(merged_data[MetricDataCalculator.INDEX])]
+        modified_type_hint_data = modified_type_hint_data[
+            ~modified_type_hint_data[MetricDataCalculator.INDEX2].isin(merged_data[MetricDataCalculator.INDEX2])]
+
         subset_merged.remove(constants.TraceData.VARTYPE)
 
         modified_type_hint_data[constants.TraceData.VARTYPE2] = modified_type_hint_data[constants.TraceData.VARTYPE]
         modified_type_hint_data = modified_type_hint_data.drop(constants.TraceData.VARTYPE, axis=1)
-        merged_data = pd.merge(original_type_hint_data, modified_type_hint_data,
-                                            on=subset_merged, how='outer')  # type: ignore
 
-        merged_data[constants.TraceData.COMPLETENESS] = ~merged_data[constants.TraceData.VARTYPE2].isna()
-        merged_data[constants.TraceData.COMPLETENESS][merged_data[constants.TraceData.VARTYPE].isna()] = None
+        original_type_hint_data[MetricDataCalculator.CUMCOUNT] = original_type_hint_data.groupby(
+            subset_merged).cumcount()
+        modified_type_hint_data[MetricDataCalculator.CUMCOUNT] = modified_type_hint_data.groupby(
+            subset_merged).cumcount()
 
-        merged_data[constants.TraceData.CORRECTNESS] = merged_data[constants.TraceData.VARTYPE] \
-                                                       == merged_data[constants.TraceData.VARTYPE2]
-        merged_data[constants.TraceData.CORRECTNESS][merged_data[constants.TraceData.VARTYPE].isna()] = None
+        merged_data2 = pd.merge(original_type_hint_data, modified_type_hint_data,
+                                on=subset_merged, how='outer')  # type: ignore
+        merged_data2 = merged_data2.drop(MetricDataCalculator.CUMCOUNT, axis=1)
+        merged_data2[constants.TraceData.CORRECTNESS] = False
+        merged_data2[constants.TraceData.COMPLETENESS] = False
+        merged_data2[constants.TraceData.COMPLETENESS][~merged_data2[constants.TraceData.VARTYPE2].isna()] = True
+        merged_data2[constants.TraceData.COMPLETENESS][merged_data2[constants.TraceData.VARTYPE].isna()] = None
+        merged_data2[constants.TraceData.CORRECTNESS][merged_data2[constants.TraceData.VARTYPE].isna()] = None
 
-        merged_data = merged_data.astype(constants.TraceData.METRICS_SCHEMA)
+        columns_in_correct_order = list(constants.TraceData.METRICS_SCHEMA.keys())
+        columns_in_correct_order.append(MetricDataCalculator.INDEX)
+        columns_in_correct_order.append(MetricDataCalculator.INDEX2)
+        merged_data2 = merged_data2[columns_in_correct_order]
+        merged_data = merged_data[columns_in_correct_order]
+        processed_data = pd.concat(
+            [merged_data, merged_data2], ignore_index=True
+        )
+        processed_data = processed_data.sort_values(by=[MetricDataCalculator.INDEX, MetricDataCalculator.INDEX2])\
+            .reset_index(drop=True)
+        processed_data = processed_data.drop([MetricDataCalculator.INDEX, MetricDataCalculator.INDEX2], axis=1)
+        processed_data = processed_data.astype(constants.TraceData.METRICS_SCHEMA)
 
-        return merged_data
+        return processed_data
 
     def get_total_completeness_and_correctness(self, metric_data: pd.DataFrame) -> tuple[float, float]:
         """Gets the total completeness & correctness of a given metric data."""
