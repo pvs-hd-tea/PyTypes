b19435aefa235debb62129ab85a2beddc2d8fb83
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-13 18:10:22 +0200

    Short script for generating config file; used by CI

---
+++ confgen/__init__.py
@@ -0,0 +1,34 @@
+import constants
+import pathlib
+import os
+from tracing import ptconfig
+
+import click
+
+@click.command(name="confgen", help="generate pytypes.toml")
+@click.option(
+    "-p",
+    "--project",
+    type=click.Path(exists=False, dir_okay=True, writable=True, path_type=pathlib.Path),
+    help="Project path (also used as output path)",
+    required=True,
+)
+def main(**params):
+    project = params["project"]
+    stdlib = pathlib.Path(pathlib.__file__).parent
+    venv = pathlib.Path(os.environ["VIRTUAL_ENV"])
+
+    assert not stdlib.is_relative_to(project), "stdlib must be outside of pytypes"
+    assert not venv.is_relative_to(project), "venv must be outside of pytypes"
+
+    cfg = ptconfig.TomlCfg(
+        pytypes=ptconfig.PyTypes(
+            project="PyTypes",
+            proj_path=project.resolve(),
+            stdlib_path=stdlib.resolve(),
+            venv_path=venv.resolve(),
+        ),
+        unifier=[],
+    )
+
+    ptconfig.write_config(project / constants.CONFIG_FILE_NAME, cfg)
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import click
 
 import fetching
 import typegen
+import confgen
 
 if __name__ == "__main__":
     # Ordered by workflow usage
-    main = click.Group(commands=[fetching.main, typegen.main])
+    main = click.Group(commands=[fetching.main, typegen.main, confgen.main])
     main()
