7a50476bd06ffd912769358c3384784799cc30e3
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-16 19:41:53 +0200

    Parametrization works

--- tests/tracing/decorators/test_pytest_features.py
+++ tests/tracing/decorators/test_pytest_features.py
@@ -1,15 +1,17 @@
 import pathlib
-
+import pytest
 
 from tracing import decorators
 
+
+##### MONKEYPATCHING #####
+
 # Adapted from https://docs.pytest.org/en/6.2.x/monkeypatch.html?highlight=mocking
-
-
 # contents of test_module.py with source code and the test
 def getssh():
     """Simple function to return expanded homedir ssh path."""
     return pathlib.Path.home() / ".ssh"
+
 
 @decorators.trace
 def test_monkeypatched_getssh(monkeypatch) -> None:
@@ -22,3 +24,30 @@ def test_monkeypatched_getssh(monkeypatch) -> None:
     # for this test with the monkeypatch.
     x = getssh()
     assert x == pathlib.Path("/abc/.ssh")
+
+
+##### PARAMETRIZE #####
+
+# Adapted from https://docs.pytest.org/en/6.2.x/parametrize.html
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 54)])
+@decorators.trace
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [("3+5", 8), ("2+4", 6), pytest.param("6*9", 42, marks=pytest.mark.xfail)],
+)
+@decorators.trace
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+@pytest.mark.parametrize("x", [0, 1])
+@pytest.mark.parametrize("y", [2, 3])
+@decorators.trace
+def test_foo(x, y):
+    assert x in (0, 1) and y in (2, 3)
