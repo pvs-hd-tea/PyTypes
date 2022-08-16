473e9b06280552c04c3326030acbce0b5af0b6ed
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-16 19:33:00 +0200

    Test monkeypatching

--- tests/tracing/decorators/test_mocks.py
+++ tests/tracing/decorators/test_mocks.py
@@ -1,18 +1,18 @@
-import logging
 import pathlib
+
+
+from tracing import decorators
 
 # Adapted from https://docs.pytest.org/en/6.2.x/monkeypatch.html?highlight=mocking
 
-from tracing import register, entrypoint
 
 # contents of test_module.py with source code and the test
 def getssh():
     """Simple function to return expanded homedir ssh path."""
     return pathlib.Path.home() / ".ssh"
 
-
-@register()
-def mocked_getssh(monkeypatch) -> bool:    
+@decorators.trace
+def test_monkeypatched_getssh(monkeypatch) -> None:
     # Application of the monkeypatch to replace Path.home
     # with the behavior to replace Path.home
     # always return '/abc'
@@ -21,8 +21,4 @@ def mocked_getssh(monkeypatch) -> bool:
     # Calling getssh() will use mockreturn in place of Path.home
     # for this test with the monkeypatch.
     x = getssh()
-    return x == pathlib.Path("/abc/.ssh")
-
-def test_mocked_getssh():
-    traced, _ = entrypoint()(lambda: None)
-    logging.debug(f"{traced}")
+    assert x == pathlib.Path("/abc/.ssh")
