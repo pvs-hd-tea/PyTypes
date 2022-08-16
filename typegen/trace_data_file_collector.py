b3c221215dbc114ff81d32b509a2ef0cb7b714c1
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-16 22:14:15 +0200

    Use constructor instead of setting attribute after initialisation

--- typegen/trace_data_file_collector.py
+++ typegen/trace_data_file_collector.py
@@ -12,12 +12,12 @@ logger = logging.getLogger(__name__)
 class DataFileCollector(ABC):
     """Collects trace data files in a given path."""
 
-    def __init__(self):
-        self.file_pattern: str = ""
+    def __init__(self, file_pattern: str):
+        self.file_pattern = file_pattern
         self.collected_data = list()
 
     def collect_data(
-            self, path: pathlib.Path, include_also_files_in_subdirectories: bool = False
+        self, path: pathlib.Path, include_also_files_in_subdirectories: bool = False
     ) -> None:
         self.collected_data.clear()
         if include_also_files_in_subdirectories:
@@ -26,12 +26,16 @@ class DataFileCollector(ABC):
             potential_trace_data_file_paths = path.glob(self.file_pattern)
         for potential_trace_data_file_path in potential_trace_data_file_paths:
             try:
-                potential_data = self._on_potential_file_path_found(potential_trace_data_file_path)
+                potential_data = self._on_potential_file_path_found(
+                    potential_trace_data_file_path
+                )
                 if potential_data is not None:
                     self.collected_data.append(potential_data)
             except Exception as exception:
                 print(exception)
-                logger.error(f"Error encountered for file: {str(potential_trace_data_file_path)}")
+                logger.error(
+                    f"Error encountered for file: {str(potential_trace_data_file_path)}"
+                )
                 logger.error(exception)
                 continue
 
@@ -44,23 +48,20 @@ class TraceDataFileCollector(DataFileCollector):
     """Collects trace data files in a given path."""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(f"*{constants.TRACE_DATA_FILE_ENDING}")
         self.trace_data = pd.DataFrame(columns=constants.TraceData.SCHEMA.keys())
         self.trace_data = self.trace_data.astype(constants.TraceData.SCHEMA)
-        file_ending = constants.TRACE_DATA_FILE_ENDING
-        self.file_pattern = "*" + file_ending
 
     def collect_data(
-            self, path: pathlib.Path, include_also_files_in_subdirectories: bool = False
+        self, path: pathlib.Path, include_also_files_in_subdirectories: bool = False
     ) -> None:
         """Collects the data in a given path."""
-        self.trace_data = pd.DataFrame(columns=constants.TraceData.SCHEMA.keys())
-        self.trace_data = self.trace_data.astype(constants.TraceData.SCHEMA)
-
         super().collect_data(path, include_also_files_in_subdirectories)
 
         if len(self.collected_data) > 0:
-            self.trace_data = pd.concat(self.collected_data, ignore_index=True, sort=False)
+            self.trace_data = pd.concat(
+                self.collected_data, ignore_index=True, sort=False
+            )
 
     def _on_potential_file_path_found(self, file_path: pathlib.Path) -> typing.Any:
         potential_trace_data = pd.read_pickle(file_path)
@@ -69,5 +70,3 @@ class TraceDataFileCollector(DataFileCollector):
         else:
             logger.info(f"Invalid column types for file: {str(file_path)}")
             return None
-
-
