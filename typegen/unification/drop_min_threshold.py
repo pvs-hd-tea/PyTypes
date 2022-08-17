a04dd5d8c74309550c22879304b42962e2d8280e
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-17 19:13:28 +0200

    flake8

--- typegen/unification/drop_min_threshold.py
+++ typegen/unification/drop_min_threshold.py
@@ -7,6 +7,7 @@ import constants
 
 class MinThresholdFilter(TraceDataFilter):
     """Drops all rows whose types appear less often than the minimum threshold."""
+
     COUNT_COLUMN = "count"
     MAX_COUNT_COLUMN = "max_count"
 
@@ -16,15 +17,16 @@ class MinThresholdFilter(TraceDataFilter):
 
     def apply(self, trace_data: pd.DataFrame) -> pd.DataFrame:
         """
-        Drops all rows whose types appear less often than the minimum threshold in the provided trace data and returns the processed trace data.
+        Drops all rows whose types appear less often than the minimum
+        threshold in the provided trace data and returns the processed trace data.
 
         @param trace_data The provided trace data to process.
         """
         subset = list(constants.AnnotationData.SCHEMA.keys())
         grouped_trace_data = (
             trace_data.groupby(subset, dropna=False)[constants.AnnotationData.VARTYPE]
-                .count()
-                .reset_index(name=MinThresholdFilter.COUNT_COLUMN)
+            .count()
+            .reset_index(name=MinThresholdFilter.COUNT_COLUMN)
         )
         joined_trace_data = pd.merge(
             trace_data, grouped_trace_data, on=subset, how="inner"
@@ -33,18 +35,27 @@ class MinThresholdFilter(TraceDataFilter):
         subset.remove(constants.AnnotationData.VARTYPE)
 
         grouped_trace_data = (
-            joined_trace_data.groupby(subset, dropna=False)[MinThresholdFilter.COUNT_COLUMN]
-                .max()
-                .reset_index(name=MinThresholdFilter.MAX_COUNT_COLUMN)
+            joined_trace_data.groupby(subset, dropna=False)[
+                MinThresholdFilter.COUNT_COLUMN
+            ]
+            .max()
+            .reset_index(name=MinThresholdFilter.MAX_COUNT_COLUMN)
         )
 
         joined_trace_data = pd.merge(
             joined_trace_data, grouped_trace_data, on=subset, how="inner"
         )
 
-        indices = joined_trace_data[MinThresholdFilter.COUNT_COLUMN] \
-                  / joined_trace_data[MinThresholdFilter.MAX_COUNT_COLUMN] > self.min_threshold
+        indices = (
+            joined_trace_data[MinThresholdFilter.COUNT_COLUMN]
+            / joined_trace_data[MinThresholdFilter.MAX_COUNT_COLUMN]
+            > self.min_threshold
+        )
         processed_data = joined_trace_data[indices]
         processed_data = processed_data.drop(
-            [MinThresholdFilter.COUNT_COLUMN, MinThresholdFilter.MAX_COUNT_COLUMN], axis=1)
-        return processed_data.reset_index(drop=True).astype(constants.AnnotationData.SCHEMA)
+            [MinThresholdFilter.COUNT_COLUMN, MinThresholdFilter.MAX_COUNT_COLUMN],
+            axis=1,
+        )
+        return processed_data.reset_index(drop=True).astype(
+            constants.AnnotationData.SCHEMA
+        )
