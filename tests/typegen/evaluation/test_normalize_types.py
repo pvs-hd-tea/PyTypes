f056e6bac90453e521fa884e0eedcba3b8d3dc0d
Viet Tran Ba <bowrecurved@gmail.com>
2022-08-18 21:08:51 +0200

    Normalize types now also normalizes inner types within types

--- tests/typegen/evaluation/test_normalize_types.py
+++ tests/typegen/evaluation/test_normalize_types.py
@@ -1,4 +1,4 @@
-from typegen.evaluation.normalize_unions import union_normalized
+from typegen.evaluation.normalize_types import normalize_type
 
 
 def test_union_normalized_returns_correct_values():
@@ -7,12 +7,16 @@ def test_union_normalized_returns_correct_values():
         ["typing.Union[typing.Union[str, int], typing.Optional[float]]", "None | float | int | str"],
         ["object | None | Optional[list[str]] | typing.Any", "None | list[str] | object | typing.Any"],
         ["typing.Optional[typing.Union[str, float]] | Union[list[list[typing.Union[float | int, str]]] | typing.Union[int, str, str | None]]",
-         "None | float | int | list[list[typing.Union[float|int,str]]] | str"]
+         "None | float | int | list[list[float | int | str]] | str"],
+        ["dict[typing.Optional[str], dict[bool | list[list[str]], Union[int, str | object]]]",
+         "dict[None | str, dict[bool | list[list[str]], int | object | str]]"]
     ]
 
     for input_expected_pair in values_to_test:
         input = input_expected_pair[0]
         expected = input_expected_pair[1]
 
-        actual = union_normalized(input)
+        actual = normalize_type(input)
+        print("Actual: " + actual)
+        print("Expected: " + expected)
         assert actual == expected
