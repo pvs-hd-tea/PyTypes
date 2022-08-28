e2b944fff94cefa68e41fcc476da05345f46ba0c
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-08-28 05:54:16 +0200

    Fix __init__

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,4 @@
 from .ptconfig import load_config, write_config
 from .resolver import Resolver
 
-__all__ = [
-    load_config, write_config
-]
+__all__ = [load_config.__name__, write_config.__name__, Resolver.__name__]
